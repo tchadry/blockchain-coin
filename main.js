5ee102883a07bee365e5ac5f4e633a2b2c17fff5
tchadry <tchadrycki@gmail.com>
2017-08-25 11:08:28 -0300

    Implemented block hash verification

--- main.js
+++ main.js
@@ -37,5 +37,21 @@ class Blockchain {
 		this.chain.push(newBlock)
 	}
 
+	isChainValid() {
+		for (let i = 1; i < this.chain.length; i++) {
+			const currentBlock = this.chain[i]
+			const previousBlock = this.chain[i - 1]
+
+			if (currentBlock.hash !== currentBlock.calculateHash) {
+				return false
+			}
+
+			if (currentBlock.previousHash !== previousBlock.hash) {
+				return false
+			}
+
+		}
+	}
+
 
 }
